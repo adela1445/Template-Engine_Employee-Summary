85a05c3dbdb8da289dd6fb36e2044b7e6fb060ca
Bluebird <stephany1445@hotmail.com>
2020-09-13 21:10:14 -0700

    App.js: added intern & engineer prompts

--- Develop/app.js
+++ Develop/app.js
@@ -12,40 +12,6 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function managerPrompt() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the Manager's name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What's your ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter your email.",
-    },
-    {
-      type: "input",
-      name: "officeNum",
-      message: "Please enter your office number.",
-    },
-    {
-      type: "rawlist",
-      name: "role",
-      message: "Which type of team membere would you like to add?",
-      choices: [
-        "Engineer",
-        "Intern",
-        "I don't want to add any more team members",
-      ],
-    },
-  ]);
-}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -66,3 +32,123 @@ function managerPrompt() {
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What's your manager's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your manager's email.",
+    },
+    {
+      type: "input",
+      name: "officeNum",
+      message: "Please enter your manager's office number.",
+    },
+    {
+      type: "rawlist",
+      name: "role",
+      message: "Which type of team member would you like to add?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I don't want to add any more team members",
+      ],
+    },
+  ])
+  .then(function (res) {
+    console.log(res);
+
+    employee.push(new Manager(res.name, res.id, res.email, res.officeNum));
+    if (res.role === Engineer) {
+      return engineerPrompt();
+    }
+    if (res.role === Intern) {
+      return internPrompt();
+    } else {
+      fs.writeFile("output/team.html", render(employee), function (err) {
+        if (err) throw err;
+      });
+    }
+  });
+
+const employee = [];
+
+function engineerPrompt() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your engineer's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What's your engineer's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your engineer's email.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your engineer's GitHub username.",
+    },
+    {
+      type: "rawlist",
+      name: "role",
+      message: "Which type of team membere would you like to add?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I don't want to add any more team members",
+      ],
+    },
+  ]);
+}
+
+function internPrompt() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your intern's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What's your intern's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your intern's email.",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter your intern's school number.",
+    },
+    {
+      type: "rawlist",
+      name: "role",
+      message: "Which type of team membere would you like to add?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I don't want to add any more team members",
+      ],
+    },
+  ]);
+}
